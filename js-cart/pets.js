3ea64a80c017ee0e618feecca3cfbffb273ab0e8
Maj11s <majharul76500@gmail.com>
2021-10-22 11:21:23 +0100

    changing var on js

--- js-cart/pets.js
+++ js-cart/pets.js
@@ -1,27 +1,27 @@
 // I have declared my variable to petlist
 var petlist = {
-  123: {
+  1: {
     // Here I have named the type of pets, description, images and the price
     name : "Ginger Hamster",
     desc : "Cute Hamster that is 10 days old",
     img : "hamster2.jpg",
     price : 40
   },
-  124: {  //This is the string of the property
+  2: {  //This is the string of the property
     // the type of pet
     name : "Grey Cat",
     desc : "Cute cat that is 1 year old",
     img : "cat2.jpg",
     price : 100
   },
-  125: {
+  3: {
     name : "Fluffy Dog",
     desc : "Beautiful Cute Dog that is 1 year old",
     img : "dog3.jpg",
     // the price of the pets
     price : 300
   },
-  126: {
+  4: {
     name : "White Hamster",
     desc : "A cute new born hamster that is 3 days old ",
     img : "hamster1.jpg",
